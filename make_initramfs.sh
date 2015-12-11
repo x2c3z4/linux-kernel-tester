d5f92bbacd4f48d079ca99c665a028f44f04ff21
vonnyfly <lifeng1519@gmail.com>
2015-12-11 22:36:20 +0800

    add initrd.img-4.2.0-1-amd64 from debian 64

--- make_initramfs.sh
+++ make_initramfs.sh
@@ -1,8 +1,10 @@
-initrd=$1 #initrd.img-3.16.0-4-amd64
-kernel_version=$2 #4.3.0-rc1+
-/bin/rm initramfs_data.cpio.gz
 cwd=`pwd`
-cp /boot/$initrd /tmp/
+kernel_version=$1 #4.3.0-rc1+
+initrd=$2
+#initrd=initramfs_data.cpio.gz #initrd.img-3.16.0-4-amd64
+#/bin/rm initramfs_data.cpio.gz
+#cp /boot/$initrd /tmp/
+cp $initrd /tmp
 /bin/rm -rf /tmp/initrd
 mkdir /tmp/initrd
 cd /tmp/initrd
@@ -10,5 +12,6 @@ zcat ../$initrd | cpio -i -d -H newc --no-absolute-filenames
 #copy modules
 /bin/rm -rf /tmp/initrd/lib/modules/*
 cp -r /lib/modules/$kernel_version /tmp/initrd/lib/modules
+#find . | cpio -o -H newc | gzip -9 > $cwd/initramfs_data.cpio.gz
 find . | cpio -o -H newc | gzip > $cwd/initramfs_data.cpio.gz
 /bin/rm -rf /tmp/initrd
